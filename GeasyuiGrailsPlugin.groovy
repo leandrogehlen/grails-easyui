64094f55b01839f0db093da6537e74466615060e
Leandro Gehlen <leandrogehlen@gmail.com>
2013-02-15 13:42:33 -0200

    Added the missing plugin settings

--- GeasyuiGrailsPlugin.groovy
+++ GeasyuiGrailsPlugin.groovy
@@ -1,19 +1,24 @@
 class GeasyuiGrailsPlugin {
 
     def version = "0.1"
-    def grailsVersion = "2.0 > *"
+    def grailsVersion = "2.0 > *"    
+    def dependsOn = [:]
 
     def pluginExcludes = [
         "web-app/css",
-		"web-app/images",
-		"web-app/js/prototype",
-		"web-app/js/application.js"
+	"web-app/images",
+	"web-app/js/prototype",
+	"web-app/js/application.js"
     ]
 
     def title = "JQuery EasyUI form Grails"
     def author = "Leandro Guindani Gehlen"
     def authorEmail = "leandrogehlen@gmail.com"
-    def description = "Supplies EasyUI resources, depends on jQuery plugin. See http://www.jeasyui.com/"
+    def description = "Supplies jQuery EasyUI resources and taglibs. Depends on jQuery EasyUI plugin. See http://www.jeasyui.com/"
+    def documentation = "http://grails.org/plugin/geasyui"
+        
 
-    def documentation = "http://grails.org/plugin/geasy-ui"
+    def license = "LGPL"    
+    def issueManagement = [system: "github", url: "https://github.com/leandrogehlen/geasyui/issues"]
+    def scm = [ url: "https://github.com/leandrogehlen/geasyui" ]
 }
