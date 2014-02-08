de35f12de74f40fcd6ce6290eaeedee1a990d7de
leandrogehlen <leandrogehlen@gmail.com>
2014-02-08 14:15:10 -0200

    Refactored scaffold javascript

--- web-app/js/easyui-scaffold.js
+++ web-app/js/easyui-scaffold.js
@@ -15,91 +15,96 @@ var scaffold = {
 	}
 }
 
-function Scaffold(options) {
-	var defaults = { 
-		window: null,		
-		route: "",
-		grid: null,
-		onBeforeEdit: function(isNewRecord, data){},	    
-	    onAfterEdit: function(isNewRecord){},
-		onBeforeRemove: function(rows){},	    
-	    onAfterRemove: function(){},
-		onBeforeRefresh: function(){},	    
-	    onAfterRefresh: function(){},
-	    onBeforeSave: function(param){},	    
-	    onAfterSave: function(){}
-	};
-	
-	var options = $.extend({}, defaults, options);
-	var self = this; 		
+var Scaffold = function (options) {
+	this.init(options);
+}
+
+Scaffold.prototype = {
 		
-	this.win = options.window;
-	this.route = options.route.replace('/index','');
-	this.frm = options.window.find('form:first');
-	this.grid = options.grid;
-	this.isNewRecord = null;
+	constructor: Scaffold,
+	
+	init : function (options) {				
+		var defaults = { 
+			window: null,		
+			route: "",
+			grid: null,
+			onBeforeEdit: function(isNewRecord, data){},	    
+			onAfterEdit: function(isNewRecord){},
+			onBeforeRemove: function(rows){},	    
+			onAfterRemove: function(){},
+			onBeforeRefresh: function(){},	    
+			onAfterRefresh: function(){},
+			onBeforeSave: function(param){},	    
+			onAfterSave: function(){}
+		};
 			
-	this.noRecordsSelectedMsg = scaffold.locale.noRecordsSelectedMsg;
-	this.onlyOneRecordSelectedMsg = scaffold.locale.onlyOneRecordSelectedMsg;
-	this.removeConfirmationMsg = scaffold.locale.removeConfirmationMsg;
-	this.alertTitle = scaffold.locale.alertTitle;
-	this.confirmTitle = scaffold.locale.confirmTitle;
-	
-	this.formLoad = function(data) {
-		if (data.success === false)
-			$.messager.alert(self.alertTitle, data.error, 'error');
-		else {
-			options.onBeforeEdit(false, data);
-			self.isNewRecord = false;
-			self.win.dialog('open');
-			options.onAfterEdit(false);
-		}
-	}			
-	
-	this.windowOpen = function() {				
-		self.frm.find('input').each(function() {																		
-	    	if ($(this).attr('type')!= 'hidden') {																				
-	        	$(this).focus();	
-	        	$(this).select();
-	        	return false;
-	    	}
+		var that = this;
+		this.options = $.extend({}, defaults, options);		
+		 				
+		this.win = options.window;
+		this.route = options.route.replace('/index','');
+		this.frm = options.window.find('form:first');
+		this.grid = options.grid;
+		this.isNewRecord = null;
+					
+		this.noRecordsSelectedMsg = scaffold.locale.noRecordsSelectedMsg;
+		this.onlyOneRecordSelectedMsg = scaffold.locale.onlyOneRecordSelectedMsg;
+		this.removeConfirmationMsg = scaffold.locale.removeConfirmationMsg;
+		this.alertTitle = scaffold.locale.alertTitle;
+		this.confirmTitle = scaffold.locale.confirmTitle;
+			
+		this.formLoad = function(data) {
+			if (data.success === false)
+				$.messager.alert(this.alertTitle, data.error, 'error');
+			else {
+				that.options.onBeforeEdit(false, data);
+				that.isNewRecord = false;
+				that.win.dialog('open');
+				that.options.onAfterEdit(false);
+			}
+		};			
+		
+		this.windowOpen = function() {				
+			that.frm.find('input').each(function() {																		
+		    	if ($(this).attr('type')!= 'hidden') {																				
+		        	$(this).focus();	
+		        	$(this).select();
+		        	return false;
+			    }
+			});		
+		};
+									
+		this.win.dialog({		
+			onOpen : that.windowOpen, 															
+			onClose: that.windowClose
+		});
+			
+		this.frm.form({
+			onLoadSuccess: that.formLoad
 		});		
-	}
+	},
 	
-	this.windowClose = function(){
-		self.clearErrors();
-	}	
-		
-	this.win.dialog({		
-		onOpen : self.windowOpen, 															
-		onClose: self.windowClose
-	});
-	
-	this.frm.form({
-		onLoadSuccess: self.formLoad
-	});
-			
-	this.validate = function(multiple) {							
-		var selected = self.grid.datagrid('getSelected');
-		var rows = self.grid.datagrid('getSelections');
+	validate: function(multiple) {							
+		var selected = this.grid.datagrid('getSelected');
+		var rows = this.grid.datagrid('getSelections');
 		var valid = false;
 
 		if (!selected)
-			$.messager.alert(self.alertTitle, self.noRecordsSelectedMsg, 'warning');
+			$.messager.alert(this.alertTitle, this.noRecordsSelectedMsg, 'warning');
 		else if ((multiple) && (rows.length > 1))
-			$.messager.alert(self.alertTitle, self.onlyOneRecordSelectedMsg, 'warning');
+			$.messager.alert(this.alertTitle, this.onlyOneRecordSelectedMsg, 'warning');
 		else
 			valid = true;
 			
 		return valid;
-	}
+	},
 		
-	this.clearErrors = function() {
+	clearErrors: function() {
 		var div = $('.row-errors:first');
 		if (div.length) div.empty();
-	}
+	},
 	
-	this.showErrors = function(errors) {
+	showErrors: function(errors) {
 		var div = $('.row-errors:first');
 		if (div.length) {						
 			var li = "";			
@@ -110,108 +115,96 @@ function Scaffold(options) {
 			
 			div.append('<ul>'+li+'</ul>');
 		}		
-	}
+	},
 		
-	this.clear = function () {
-		self.frm.find('input,select,textarea').each(function(){			
-			var t = this.type, tag = this.tagName.toLowerCase();
-			if (t == 'text' || t == 'hidden' || t == 'password' || tag == 'textarea'){
-				this.value = '';
-			} else if (t == 'file'){
-				var file = $(this);
-				file.after(file.clone().val(''));
-				file.remove();
-			} else if (t == 'checkbox' || t == 'radio'){
-				this.checked = false;
-			} else if (tag == 'select'){
-				this.selectedIndex = -1;
-			}			
-		});
-						
-		if ($.fn.combo) self.frm.find('.combo-f').combo('clear');
-		if ($.fn.combobox) self.frm.find('.combobox-f').combobox('clear');
-		if ($.fn.combotree) self.frm.find('.combotree-f').combotree('clear');
-		if ($.fn.combogrid) self.frm.find('.combogrid-f').combogrid('clear');		
-	}
-			
-	this.add = function() {																	
-		self.clear();
-		self.isNewRecord = true;
-		options.onBeforeEdit(true, {});
-    	self.win.dialog('open');    
-    	options.onAfterEdit(true);
-	}
+	clear: function () {				
+		this.frm.form('clear');												
+		if ($.fn.combo) this.frm.find('.combo-f').combo('clear');
+		if ($.fn.combobox) this.frm.find('.combobox-f').combobox('clear');
+		if ($.fn.combotree) this.frm.find('.combotree-f').combotree('clear');
+		if ($.fn.combogrid) this.frm.find('.combogrid-f').combogrid('clear');		
+	},
+		
+	add: function() {											
+		this.clear();		
+		this.isNewRecord = true;		
+		this.options.onBeforeEdit(true, {});		
+		this.win.dialog('open');    
+		this.options.onAfterEdit(true);
+	},
 	
-	this.edit = function() {		
-		if (self.validate(true)) {						
-			var url = self.route+"/"+ scaffold.route.show +"/"+self.grid.datagrid('getSelected').id+'.json';			
-			self.frm.form('reset');		
-			self.frm.form('load', url);			
+	edit: function() {		
+		if (this.validate(true)) {						
+			var url = this.route+"/"+ scaffold.route.show +"/"+this.grid.datagrid('getSelected').id+'.json';			
+			this.frm.form('reset');		
+			this.frm.form('load', url);			
 		}
-	} 
+	},
 	
-	this.remove = function () {		
-		if (self.validate(false)) {
-			$.messager.confirm(self.confirmTitle, self.removeConfirmationMsg, function(r){			
+	remove: function () {		
+		if (this.validate(false)) {
+			var that = this;
+			
+			$.messager.confirm(this.confirmTitle, this.removeConfirmationMsg, function(r){			
 				if (r) {
-					var rows = self.grid.datagrid('getSelections');		
-					options.onBeforeRemove(rows);
-					for(var i in rows) {							
-						$.post(self.route +'/'+ scaffold.route.remove+'/'+rows[i].id, function(data, status) {							
+					var rows = that.grid.datagrid('getSelections');		
+					that.options.onBeforeRemove(rows);
+					for(var i in rows) {
+						console.log(that.route +'/'+ scaffold.route.remove+'/'+rows[i].id);
+						$.post(that.route +'/'+ scaffold.route.remove+'/'+rows[i].id, function(data, status) {							
 							if (status != "success") {
-								$.messager.alert(self.alertTitle, data.error, 'error');
+								$.messager.alert(this.alertTitle, data.error, 'error');
 							}
 							else {
-								self.grid.datagrid('reload');
-								self.grid.datagrid('clearSelections');
+								that.grid.datagrid('reload');
+								that.grid.datagrid('clearSelections');
 							}
 						});
 					}
-					options.onAfterRemove();
+					that.options.onAfterRemove();
 				}							
 			}); 				
 		}
-	}		
+	},
 	
-	this.refresh = function() {		
-		options.onBeforeRefresh();
-		self.grid.datagrid('reload');
-		self.grid.datagrid('clearSelections');
-		options.onAfterRefresh();
-	} 	
-	
-	this.save = function() {		
-		if (self.frm.form('validate')) {			
-			self.clearErrors();
-								
-			var url = (self.isNewRecord) ? scaffold.route.create : scaffold.route.update;						
-			var param = {}
+	save: function() {		
+		if (this.frm.form('validate')) {			
+			this.clearErrors();
+			
+			var that = this,
+				url = (this.isNewRecord) ? scaffold.route.create : scaffold.route.update,						
+				param = {};
 						
-			options.onBeforeSave(param);			
+			this.options.onBeforeSave(param);			
 			param = (!$.isEmptyObject(param)) ? '&' + $.param(param, true) : '';		
 						
-			$.post(this.route+'/'+url+'.json', self.frm.serialize() + param, function(data, status) {
-				console.log(data);
-				console.log(status);
+			$.post(this.route+'/'+url+'.json', this.frm.serialize() + param, function(data, status) {				
 				if (status == "success") {			
-					options.onAfterSave();
-					self.win.window('close');
-					self.grid.datagrid('reload');
+					that.options.onAfterSave();
+					that.win.window('close');
+					that.grid.datagrid('reload');
 				}							
 			}).fail(function(xhr, status) {					
 				if (xhr.status == 422) {
-					self.showErrors($.parseJSON(xhr.responseText).errors);															
+					that.showErrors($.parseJSON(xhr.responseText).errors);															
 				} else {								
-					$.messager.alert(self.alertTitle, xhr.responseText, 'error');
+					$.messager.alert(that.alertTitle, xhr.responseText, 'error');
 				}
 					
 			});
 		}		
-	}
+	},
 	
-	this.search = function(name, value) {				
-		self.grid.datagrid('clearSelections');				
-		self.grid.datagrid('options').queryParams = {'field': name, 'value': value};		
-		self.grid.datagrid('load');		
-	}
+	refresh: function() {		
+		this.options.onBeforeRefresh();
+		this.grid.datagrid('reload');
+		this.grid.datagrid('clearSelections');
+		this.options.onAfterRefresh();
+	},
+	
+	search: function(name, value) {				
+		this.grid.datagrid('clearSelections');				
+		this.grid.datagrid('options').queryParams = {'field': name, 'value': value};		
+		this.grid.datagrid('load');		
+	}		
 }
